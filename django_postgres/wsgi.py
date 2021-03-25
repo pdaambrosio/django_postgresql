87d2a8d614dc3e3c93fd0daeadc1076f82b2976f
Paulo Daniel Ambrosio dos Santos <pda.ambrosio@gmail.com>
2021-03-24 22:53:39 -0300

    class based views

--- django_postgres/wsgi.py
+++ django_postgres/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_postgre project.
+WSGI config for django_postgres project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_postgre.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_postgres.settings')
 
 application = get_wsgi_application()
