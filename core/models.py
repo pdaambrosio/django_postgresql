10c3a56d7edea42fa887f168e8a8c8e9b3cdcf44
Paulo Daniel Ambrosio dos Santos <pda.ambrosio@gmail.com>
2021-03-29 20:24:16 -0300

    included class models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from stdimage.models import StdImageField
 
-# Create your models here.
+class Base(models.Model):
+    create = models.DateField('Created', auto_now_add=True)
+    modified = models.DateField('Updated', auto_now=True)
+    active = models.BooleanField('active?', default=True)
+
+    class Meta:
+        abstract = True
