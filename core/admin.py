f27d9b81cf339d5ec9aeedd1bb00655704e4fa0c
Paulo Daniel Ambrosio dos Santos <pda.ambrosio@gmail.com>
2021-04-04 19:36:00 -0300

    admin pages created

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Service, Position, Team
 
-# Register your models here.
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('position', 'active', 'modified')
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('service', 'icons', 'active', 'modified')
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position', 'active','modified')
