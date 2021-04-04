2c70bad03e59b941c646d5c36484b3d5f6befc73
Paulo Daniel Ambrosio dos Santos <pda.ambrosio@gmail.com>
2021-04-04 19:20:20 -0300

    Create model Position, Team and Service

--- core/models.py
+++ core/models.py
@@ -28,3 +28,29 @@ class Service(Base):
 
     def __str__(self):
         return self.service
+
+class Position(Base):
+    position = models.CharField('Position', max_length=100)
+
+    class Meta:
+        verbose_name = 'Position'
+        verbose_name_plural = 'Positions'
+
+    def __str__(self):
+        return self.cargo
+
+class Team(Base):
+    name = models.CharField('Name', max_length=100)
+    position = models.ForeignKey('core.Position', verbose_name='Position', on_delete=models.CASCADE)
+    bio = models.TextField('Bio', max_length=200)
+    image = StdImageField('Image', upload_to='team', variations={'thumb':{'width': 480, 'height': 480, 'crop': True}})
+    facebook = models.CharField('Facebook', max_length=100, default='#')
+    twitter = models.CharField('Twitter', max_length=100, default='#')
+    instagram = models.CharField('Instagram', max_length=100, default='#')
+
+    class Meta:
+        verbose_name = 'Team'
+        verbose_name_plural = 'Teams'
+
+    def __str__(self):
+        return self.nome
