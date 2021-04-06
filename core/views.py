0674f519ba6877011eb782fe89e364036f5961f4
Paulo Daniel Ambrosio dos Santos <pda.ambrosio@gmail.com>
2021-04-05 21:36:41 -0300

    updated views with context

--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from .models import Service, Team
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(***kwargs)
+        context['Service'] = Service.objects.all()
+        context['Team'] = Team.objects.all()
+        return context
