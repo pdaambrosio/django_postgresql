8e7067648731683e3bc10d0c268c3da04dd79436
Paulo Daniel Ambrosio dos Santos <pda.ambrosio@gmail.com>
2021-04-11 20:48:18 -0300

    form contact

---
+++ core/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.core.mail.message import EmailMessage
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=100)
+    email = forms.EmailField(label='E-mail', max_length=100)
+    subject = forms.CharField(label='Assunto', max_length=100)
+    message = forms.CharField(label='Message', widget=forms.Textarea())
+
+    def send_mail(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        subject = self.cleaned_data['subject']
+        message = self.cleaned_data['message']
+
+        contents = f'Name: {name}\nE-mail: {email}\nSubject: {subject}\nMessage {message}'
+
+        mail = EmailMessage(
+            subject=subject
+            body=contents
+            from_email=email
+            to=admin@website.com
+            headers=('Reply-To': email)
+        )
+        mail.send()
