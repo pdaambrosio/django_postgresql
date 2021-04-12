cab3ecb0df4f0b3738b018b845a662e3e2537ba2
Paulo Daniel Ambrosio dos Santos <pda.ambrosio@gmail.com>
2021-04-11 21:54:38 -0300

    mail commented

--- django_postgres/settings.py
+++ django_postgres/settings.py
@@ -119,4 +119,4 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
